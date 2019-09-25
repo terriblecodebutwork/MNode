7e5688beefc1d22acfa3998464291ac119ff9e94
terriblecodebutwork <metanet@yeah.net>
2019-09-25 16:54:41 +0800

    resync all utxo

--- lib/bex_web/live/index_live.ex
+++ lib/bex_web/live/index_live.ex
@@ -36,84 +36,66 @@ defmodule BexWeb.IndexLive do
   def render(assigns) do
     ~L"""
     <%= if @private_keys == [] do %>
-    <a href="/private_keys/new" >Import PrivateKey</a>
-    <% else %>
+      <a href="/private_keys/new" >Import PrivateKey</a>
+      <% else %>
 
-    <div class="row">
-      <div class="column">
+      <div>
         <h3>Value of one Coin: <%= @coin_sat %></h3>
-      </div>
-      <div class="column">
         <form phx-submit="set_coin_sat">
-          <div class="row">
-            <div class="column">
               <input name="value" type="number">
-            </div>
-            <div class="column">
               <button type="submit">Set</button>
-            </div>
-          </div>
         </form>
       </div>
-    </div>
 
-    <ul>
-      <%= for k <- @private_keys || [] do %>
-        <div style="border: 1px dotted gray; padding: 5px">
+      <div>
+        <button phx-click="resync_all">Resync all UTXOs</button>
+      </div>
 
-          <div class="row">
-            <div class="column">
-              <h2><%= k.address %></h2>
-            </div>
-            <div class="column">
-              <%= if is_nil(k.base_key_id) do %>
-              <button phx-click="meta" phx-value-id="<%= k.id %>" >Metanet</button>
-              <% end %>
-            </div>
-          </div>
-
-          <div class="row">
-            <div class="column">
-                  <%= for t <- [:dust, :permission, :gold, :coin] do
-                        "#{t}: #{Enum.count(k.utxos, fn x -> x.type == t end)}, "
-                      end |> Enum.join() %>
-            </div>
-          </div>
-
-          <%= if @loading do %>
-          <h4>Loading...</h4>
-          <% end %>
-
-          <div class="row">
-            <div class="column">
-              <button phx-click="resync_utxo" phx-value-id="<%= k.id %>" >ReSync UTXOs</button>
-            </div>
-            <div class="column">
-              <button phx-click="recast" phx-value-id="<%= k.id %>">Recast</button>
-            </div>
-            <div class="column">
-              <button phx-click="mint_all" phx-value-id="<%= k.id %>">Mint</button>
-            </div>
-            <div class="column">
-              <button phx-click="show_utxos" phx-value-id="<%= k.id %>">show/hide</button>
-            </div>
-          </div>
-
-          <%= if k.id in @showing do %>
-          <div>
-              <%= for u <- Enum.sort_by(k.utxos, fn u -> u.value end, &>=/2) || [] do %>
-                <ul>
-                    <li>type: <%= u.type %></li>
-                    <li>txid: <%= u.txid %></li>
-                    <li>value: <%= u.value %></li>
-                </ul>
-              <% end %>
-          </div>
-          <% end %>
-
-        </div>
+      <%= if @loading do %>
+      <h4>Loading...</h4>
       <% end %>
-    </ul>
+
+      <div>
+        <table border="1">
+          <tr>
+            <th>address</th>
+            <th>metanet</th>
+            <th>coin</th>
+            <th>permission</th>
+            <th>gold</th>
+            <th>dust</th>
+            <th>commands</th>
+          </tr>
+        <%= for k <- @private_keys || [] do %>
+          <tr>
+            <td><a href="https://whatsonchain.com/address/<%= k.address %>" target="_blank"><pre><%= k.address %></pre></a></td>
+            <td><%= if is_nil(k.base_key_id) do %>
+              <button phx-click="meta" phx-value-id="<%= k.id %>" >Metanet</button>
+            <% end %></td>
+            <%= for t <- [:coin, :permission, :gold, :dust] do %>
+            <td><%= Enum.count(k.utxos, fn x -> x.type == t end) %></td>
+            <% end %>
+            <td>
+              <button phx-click="resync_utxo" phx-value-id="<%= k.id %>" >ReSync UTXOs</button>
+              <button phx-click="recast" phx-value-id="<%= k.id %>">Recast</button>
+              <button phx-click="mint_all" phx-value-id="<%= k.id %>">Mint</button>
+              <button phx-click="show_utxos" phx-value-id="<%= k.id %>">show/hide</button>
+            </td>
+
+          </tr>
+          <%= if k.id in @showing do %>
+            <%= for u <- Enum.sort_by(k.utxos, fn u -> u.value end, &>=/2) || [] do %>
+            <tr>
+              <td>type: <%= u.type %></td>
+              <td>txid: <%= u.txid %></td>
+              <td>value: <%= u.value %></td>
+            </tr>
+            <% end %>
+          <% end %>
+
+        <% end %>
+        </table>
+      </div>
     <% end %>
     """
   end
@@ -128,10 +110,23 @@ defmodule BexWeb.IndexLive do
     {:noreply, reload(socket)}
   end
 
+  def handle_event("resync_all", _, socket) do
+    send(self(), "resync_all")
+    {:noreply, assign(socket, :loading, true)}
+  end
+
   def handle_event(cmd, %{"id" => id}, socket) do
     id = String.to_integer(id)
     send(self(), {cmd, id})
     {:noreply, assign(socket, :loading, true)}
+  end
+
+  def handle_info("resync_all", socket) do
+    list = socket.assigns.private_keys
+    for x <- list do
+      Wallet.sync_utxos_of_private_key(x)
+    end
+    {:noreply, reload(socket)}
   end
 
   def handle_info({"recast", id}, socket) do
