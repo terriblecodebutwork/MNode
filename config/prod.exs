2aed4e1fa4b1fe9aec7ca253263b2c60a33691ae
terriblecodebutwork <metanet@yeah.net>
2019-10-01 12:49:00 +0800

    change logger level in prod

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ config :bex, BexWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
-config :logger, level: :info
+config :logger, level: :warn
 
 # tell logger to load a LoggerFileBackend processes
 config :logger,
