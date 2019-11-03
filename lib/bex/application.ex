f182cab07d72e5ecbf950dacaafe830ae2c52e46
terriblecodebutwork <metanet@yeah.net>
2019-11-03 17:01:37 +0800

    option in merkle saver

--- lib/bex/application.ex
+++ lib/bex/application.ex
@@ -15,12 +15,15 @@ defmodule Bex.Application do
         Bex.CoinManager,
         # BsvNews,
         Bex.KV,
-        Bex.Store.MerkleSaver,
         # Start the endpoint when the application starts
         BexWeb.Endpoint
         # Starts a worker by calling: Bex.Worker.start_link(arg)
         # {Bex.Worker, arg},
-      ] ++ if System.get_env("BEX_BROADCASTER"), do: [Bex.Broadcaster], else: []
+      ] ++ (
+        if System.get_env("BEX_BROADCASTER"), do: [Bex.Broadcaster], else: []
+      ) ++ (
+        if System.get_env("BEX_MERKLESAVER"), do: [Bex.Store.MerkleSaver], else: []
+      )
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
